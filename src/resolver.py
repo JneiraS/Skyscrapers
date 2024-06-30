1e7ad6d6f8ffa3604803a8f34266b6ebdbeae69b
jneira <johnny.neira@gmail.com>
2024-06-30 19:21:52 +0200

    some new tests docstrings and refactorings

--- src/resolver.py
+++ src/resolver.py
@@ -1,6 +1,7 @@
 import random
 from itertools import permutations
 from src.grid import transpose, clues, indice_adder, Grid
+from src.ui import grid_printer
 
 results_to_insert: list = []
 grid_filled: list = []
@@ -8,10 +9,10 @@ grid_filled: list = []
 
 def permutations_possibles(grid_seize: int):
     """
-    Génère toutes les permutations possibles de hauteurs de bâtiments pour un grille de taille 'grid_seize'.
+    Génère toutes les permutations possibles de hauteurs de bâtiments pour une grille de taille 'grid_seize'.
     Cette fonction crée une liste de hauteurs de bâtiments allant de 1 à 'grid_seize', puis génère toutes les
     permutations possibles de ces hauteurs.
-    Ces permutations représentent toutes les configurations possibles de hauteurs de bâtiments dans un grille donné.
+    Ces permutations représentent toutes les configurations possibles de hauteurs de bâtiments dans une grille donnée.
 
     :param grid_seize:
     :return list: Une liste de tuples, où chaque tuple représente une permutation possible des hauteurs de
@@ -29,8 +30,8 @@ def replace_zeros_with_list_of_possibilities(original_list: list, remaining_poss
     :param original_list: La liste originale dont certains éléments doivent être remplacés par remaining_possibilities.
     remaining_possibilities (any): L'élément avec lequel remplacer les zéros dans original_list.
     :param remaining_possibilities:  L'élément avec lequel remplacer les zéros dans original_list.
-    :return list: Une nouvelle liste où tous les zéros ont été remplacés par remaining_possibilities, tandis que les autres
-     valeurs restent inchangées.
+    :return list: Une nouvelle liste où tous les zéros ont été remplacés par remaining_possibilities, tandis que les
+    autres valeurs restent inchangées.
     """
     return [remaining_possibilities if e == 0 else e for e in original_list]
 
@@ -43,44 +44,71 @@ def replace_list_of_possibilities_whit_0(original_list):
 
     :param original_list: La liste originale dont certains éléments (qui sont eux-mêmes des listes) doivent être
      remplacés par 0.
-    :return list: Une nouvelle liste où tous les éléments qui étaient des listes ont été remplacés par 0, tandis que les autres valeurs restent inchangées.
+    :return list: Une nouvelle liste où tous les éléments qui étaient des listes ont été remplacés par 0, tandis que
+    les autres valeurs restent inchangées.
     """
     return [0 if isinstance(e, list) else e for e in original_list]
 
 
 def determine_last_line(list_to_analyse: list):
-    perms = permutations_possibles(4)
+    """
+    Filtre les permutations possibles pour trouver celles qui correspondent à une configuration spécifique de la
+    dernière ligne d’une grille.
+    Cette fonction prend en entrée une liste représentant la configuration de la dernière ligne d’une grille et filtre
+    les permutations possibles de hauteurs de bâtiments pour une grille de taille 4x4, afin de ne garder que celles
+    qui correspondent à la configuration spécifiée pour la dernière ligne, en tenant compte des conditions spécifiées
+    pour chaque côté de la ligne.
 
+    :param list_to_analyse: Liste représentant la configuration de la dernière ligne de la grille, où chaque
+    élément de la liste est une hauteur de bâtiment et 'zéro' indique l'absence de bâtiment.
+    :return list: Une liste filtrée de permutations possibles qui correspondent à la configuration spécifiée pour la
+    dernière ligne de la grille.
+    """
+    perms: list = permutations_possibles(4)
     for j in range(1, 4):
-        for i in range(4):
+        for side in range(4):
             perms = [
-                line
-                for line in perms
-                if line[i] != list_to_analyse[j][i + 1]
-                   or list_to_analyse[j][i + 1] == 0
+                line for line in perms if
+                line[side] != list_to_analyse[j][side + 1] or list_to_analyse[j][side + 1] == 0
             ]
-
-    for i in range(4):
+    for side in range(4):
         perms = [
             line
             for line in perms
-            if line[i] == list_to_analyse[4][i + 1] or list_to_analyse[4][i + 1] == 0
+            if line[side] == list_to_analyse[4][side + 1] or list_to_analyse[4][side + 1] == 0
         ]
-
     return perms
 
 
-def generate_game_configurations(game_grid: list):
-    grid_dimension = len(game_grid) - 2
-    modified_new_grid = []
-    modified_new_grid2 = []
+def heuristics_determination(game_grid: list, grid_dimension: int):
+    """
+    Applique une heuristique de détermination des hauteurs de bâtiments pour une grille de jeu, en ajustant les
+    hauteurs des bâtiments selon certaines règles et en transposant la grille.
+    Cette fonction modifie la grille de jeu en fonction de ses dimensions actuelles, en ajustant les hauteurs des
+    bâtiments pour optimiser une certaine condition (par exemple, maximiser la différence entre les hauteurs des
+    bâtiments adjacents). Elle applique ensuite une transformation de la grille (transpose) pour préparer la grille à
+    une nouvelle analyse.
+    :param game_grid: (list) La grille de jeu sous forme de liste de listes, où chaque sous-liste représente une
+    ligne de la grille.
+    :param grid_dimension: (int): La dimension de la grille, c'est-à-dire le nombre maximum de niveaux de hauteur
+    possible pour les bâtiments.
+    :return: (tuple): Un tuple contenant deux éléments:
+        - Une collection (set) des hauteurs de bâtiments possibles après l'application de l'heuristique.
+    - La grille de jeu transposée, prête à une nouvelle analyse ou simulation.
 
-    base = {i for i in range(grid_dimension + 1)}
+    Notes: Cette fonction suppose que la fonction `transpose` est déjà définie et capable de transposer
+    la grille de jeu.
 
-    for i in range(4):
+    """
+    base = {possible_building_heights for possible_building_heights in range(grid_dimension + 1)}
+    for side_of_grid in range(4):
 
         for row in game_grid:
-            if row[0] == 1:
+            if row[0] == 1 and row[-1] == 2:
+                row.pop(-2)
+                row.insert(-1, grid_dimension - 1)
+
+            elif row[0] == 1:
                 row.pop(1)
                 row.insert(1, grid_dimension)
 
@@ -89,18 +117,23 @@ def generate_game_configurations(game_grid: list):
                     row.pop(j)
                     row.insert(j, j)
 
-            elif row[0] == 1 and row[-1] == 2:
-                row.pop(-2)
-                row.insert(-1, grid_dimension - 1)
-
         game_grid = transpose(game_grid)
+    return base, game_grid
+
+
+def generate_game_configurations(game_grid: list):
+    grid_dimension = len(game_grid) - 2
+    modified_grid = []
+    modified_grid_2 = []
+
+    base, game_grid = heuristics_determination(game_grid, grid_dimension)
 
     for row in game_grid:
         remaining_possibilities = list(base - {e for e in row[1:-1]})
-        modified_new_grid.append(replace_zeros_with_list_of_possibilities(row, remaining_possibilities))
+        modified_grid.append(replace_zeros_with_list_of_possibilities(row, remaining_possibilities))
 
-    for i in range(4):
-        for row in modified_new_grid:
+    for side_of_grid in range(4):
+        for row in modified_grid:
             if row[0] == 1 and row[-1] == 3 and row[1] == 4 and row[2] == 3:
                 temp_element = row.pop(-2)
 
@@ -109,42 +142,39 @@ def generate_game_configurations(game_grid: list):
                     row.insert(-1, num)
                 except IndexError:
                     row.insert(-1, temp_element)
-        modified_new_grid = transpose(modified_new_grid)
+        modified_grid = transpose(modified_grid)
 
-    for row in modified_new_grid:
-        modified_new_grid2.append(replace_list_of_possibilities_whit_0(row))
-    return modified_new_grid2
-
-
-def decalage(grid_to_decale: list, origine: int, destination: int):
-    temp_line = grid_to_decale.pop(origine)
-    grid_to_decale.insert(destination, temp_line)
+    for row in modified_grid:
+        modified_grid_2.append(replace_list_of_possibilities_whit_0(row))
+    return modified_grid_2
 
 
-def adaptive_game_grid_generation(game_grid):
-    grid_with_euristics = generate_game_configurations(game_grid)
+def position_change(grid_to_change: list, origine: int, destination: int):
+    temp_line = grid_to_change.pop(origine)
+    grid_to_change.insert(destination, temp_line)
 
-    empty_grid = []
 
-    for _ in range(4):
-        last_line_result = determine_last_line(grid_with_euristics)
-        empty_grid.append(last_line_result)
-        line_two = grid_with_euristics.pop(1)
-        grid_with_euristics.insert(4, line_two)
+def fill_grid_with_choices(empty_grid_to_fill, possibles_results_by_line):
+    """
+    Remplit une grille vide avec des choix provenant d'une liste de résultats possibles.
 
-    possibles_results_by_line = empty_grid.copy()
+    Cette fonction itère sur une liste de résultats possibles, sélectionnant un choix aléatoire de chacun. Elle vérifie
+    ensuite
+    si l'élément sélectionné répond à une condition spécifique dans la grille vide à remplir. Si la condition est
+    remplie, l'élément
+    sélectionné est ajouté à la nouvelle grille.
 
-    line_two = possibles_results_by_line.pop(0)
-    possibles_results_by_line.insert(2, line_two)
+    Args:
+        empty_grid_to_fill (list): Une grille vide représentée comme une liste de listes, prête à être remplie avec des
+        choix.
+        possibles_results_by_line (list): Une liste de résultats possibles, où chaque élément représente un choix
+        potentiel pour remplir la grille.
 
-    line_two = possibles_results_by_line.pop(0)
-    possibles_results_by_line.insert(1, line_two)
-
-    buildings = random.choice(possibles_results_by_line[0])
-    empty_grid_to_fill = [list(buildings)]
-
-    for i in possibles_results_by_line[1:]:
-        k = random.choice(i)
+    Returns:
+        None: Modifie le paramètre empty_grid_to_fill en place.
+    """
+    for possible_result in possibles_results_by_line[1:]:
+        k = random.choice(possible_result)
         for j in range(4):
             if empty_grid_to_fill[0][j] == k[j]:
                 pass
@@ -152,25 +182,68 @@ def adaptive_game_grid_generation(game_grid):
                 empty_grid_to_fill.append(list(k))
                 break
 
-    line_two = empty_grid_to_fill.pop(0)
-    empty_grid_to_fill.insert(1, line_two)
 
-    line_two = empty_grid_to_fill.pop(2)
-    empty_grid_to_fill.insert(3, line_two)
+def adjust_game_board(game_grid):
+    """
+    Génère une grille de jeu adaptative en appliquant des heuristiques et en ajustant les configurations de jeu.
+
+    Cette fonction commence par générer des configurations de jeu à partir de la grille de jeu initiale en utilisant une
+    fonction d'heuristique. Ensuite, elle adapte la grille générée en ajoutant et en supprimant des lignes spécifiques
+    pour optimiser certaines conditions de jeu. Des changements de position sont appliqués pour diversifier les
+    résultats possibles par ligne.
+    Enfin, une grille vide est remplie avec des choix aléatoires basés sur les résultats possibles, avec des ajustements
+    supplémentaires pour optimiser davantage la configuration finale.
+
+    Args:
+        game_grid (list): La grille de jeu initiale sous forme de liste de listes, où chaque sous-liste représente une
+        ligne de la grille.
+
+    Returns:
+        list: Une grille de jeu adaptative et optimisée, prête à être utilisée pour le jeu.
+
+    Notes:
+        Cette fonction suppose que plusieurs fonctions auxiliaires sont déjà définies et capables de réaliser les tâches
+        suivantes :
+        - generate_game_configurations: Génère des configurations de jeu à partir de la grille de jeu initiale.
+        - determine_last_line: Détermine la dernière ligne de la grille générée selon certaines heuristiques.
+        - position_change: Change la position d'un élément dans une liste pour diversifier les résultats possibles.
+        - fill_grid_with_choices: Remplit une grille vide avec des choix aléatoires basés sur les résultats possibles.
+
+    :param game_grid:
+    :return:
+    """
+    grid_with_heuristics = generate_game_configurations(game_grid)
+
+    empty_grid = []
+
+    for _ in range(4):
+        last_line_result = determine_last_line(grid_with_heuristics)
+        empty_grid.append(last_line_result)
+        line_two = grid_with_heuristics.pop(1)
+        grid_with_heuristics.insert(4, line_two)
+
+    possibles_results_by_line = empty_grid.copy()
+    position_change(possibles_results_by_line, 0, 2)
+    position_change(possibles_results_by_line, 0, 1)
+    buildings = random.choice(possibles_results_by_line[0])
+    empty_grid_to_fill = [list(buildings)]
+    fill_grid_with_choices(empty_grid_to_fill, possibles_results_by_line)
+    position_change(empty_grid_to_fill, 0, 1)
+    position_change(empty_grid_to_fill, 2, 3)
+
     return empty_grid_to_fill
 
 
 def rotate_lists_of_lists(liste):
     """
-    Effectue une rotation de toutes les sous-listes dans une liste de listes.
+    Effectue une rotation de toutes les sous-listes dans une liste de listes dans le sens anti-horaire.
 
     Args:
-    - liste : Liste de listes dont les sous-listes doivent être rotées.
-
+    - liste: Liste de listes dont les sous-listes doivent être pivotées.
     Returns:
-    - Une nouvelle liste de listes avec toutes les sous-listes rotées.
+    - Une nouvelle liste de listes avec toutes les sous-listes pivotées.
     """
-    return [[liste[j][i] for j in range(len(liste))] for i in range(len(liste[0]))]
+    return [[liste[j][k] for j in range(len(liste))] for k in range(len(liste[0]))]
 
 
 grid = [
@@ -184,7 +257,7 @@ grid = [
 
 while True:
 
-    original = adaptive_game_grid_generation(grid)
+    original = adjust_game_board(grid)
     tocheckt = rotate_lists_of_lists(original)
     container = []
 
@@ -204,4 +277,5 @@ for _ in range(4):
     indices = clues(gridd)
     gridd = transpose(indice_adder(indices, gridd))
 
-# grid_printer(gridd)
+grid_printer(gridd)
+
--- src/test/test_resolver.py
+++ src/test/test_resolver.py
@@ -1,18 +1,42 @@
 from unittest import TestCase
 
-from src.resolver import permutations_possibles, replace_zeros_with_list_of_possibilities, replace_list_of_possibilities_whit_0
+from src.resolver import (permutations_possibles, replace_zeros_with_list_of_possibilities,
+                          replace_list_of_possibilities_whit_0, determine_last_line, rotate_lists_of_lists)
+
+# ID:33884
+grid = [
+    [-1, 2, 1, 2, 4, -1],
+    [2, 3, 4, 2, 1, 3],
+    [1, 4, 3, 1, 2, 3],
+    [3, 1, 2, 4, 3, 2],
+    [3, 0, 0, 0, 0, 1],
+    [-1, 2, 4, 2, 1, -1],
+]
 
 
 class Test(TestCase):
     def test_permutations_possible(self):
-        results = permutations_possibles(3)
-        self.assertTrue(type(results) is list)
-        self.assertEqual(results, [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)])
+        result = permutations_possibles(3)
+        self.assertTrue(type(result) is list)
+        self.assertEqual(result, [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)])
 
     def test_replace_zeros_with_list_of_possibilities(self):
-        results = replace_zeros_with_list_of_possibilities([0, 1, 2, 0], ['x'])
-        self.assertEqual(results, [['x'], 1, 2, ['x']])
+        result: list = replace_zeros_with_list_of_possibilities([0, 1, 2, 0], ['x'])
+        self.assertEqual(result, [['x'], 1, 2, ['x']])
 
     def test_replace_list_of_possibilities_whit_0(self):
-        results = replace_list_of_possibilities_whit_0([0, [1, 2], 3])
-        self.assertEqual(results,[0, 0, 3])
+        result: list = replace_list_of_possibilities_whit_0([0, [1, 2], 3])
+        self.assertEqual(result, [0, 0, 3])
+
+    def test_determine_last_line(self):
+        result: list = determine_last_line(grid)
+        self.assertEqual(result, [(2, 1, 3, 4)])
+
+    def test_rotate_lists_of_lists(self):
+        result = rotate_lists_of_lists(grid)
+        self.assertEqual(result, [[-1, 2, 1, 3, 3, -1],
+                                  [2, 3, 4, 1, 0, 2],
+                                  [1, 4, 3, 2, 0, 4],
+                                  [2, 2, 1, 4, 0, 2],
+                                  [4, 1, 2, 3, 0, 1],
+                                  [-1, 3, 3, 2, 1, -1]])
